f5abfab76f8c76732989a14b5595f48f8eb44ab1
oleg14038 <119931573+oleg14038@users.noreply.github.com>
2023-04-30 22:15:03 +0900

    cals

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+def canculator(num, num2, operator):
+    if operator == '+':
+        print('sum', num + num2) # 
+    elif operator == '-': 
+        print('Subtraction [səbˈtrakSH(ə)n]' , num - num2)
+    elif operator == '*': 
+        print('Multiplication [məltəpləˈkāSH(ə)n]', num * num2)
+    elif operator == '/': 
+        print('Division [dəˈviZHən] [dɪˈvɪʒən]', num / num2)
+
+
+
+    
+while True: # До : загаловок, а после инструкция 
+
+    num = float(input('num1'))
+    operator = input('operator')
+    num2 = float(input('num2'))
+    canculator(num, num2, operator)
+
+   
